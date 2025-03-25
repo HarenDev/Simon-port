9fae7410919f34f943667b86ec7777694ff565a3
HarenDev <157764758+HarenDev@users.noreply.github.com>
2025-03-25 16:10:54 -0400

    Added some comments and general cleanup

--- src/Button.cpp
+++ src/Button.cpp
@@ -8,21 +8,23 @@ Button::Button(float x, float y, float width, float height, string imagePath, st
 
     isPressed = false;
 
+    //If the image path is correct, then it will load the image
     if (raylib::FileExists(imagePath)){
         image.Load(raylib::Image(imagePath).Resize(width, height));
     }
 
+    //If the sound path is correct, then it will load the sound
     if (raylib::FileExists(soundPath)){
         buttonSound.Load(soundPath);
     }
 }
 
 void Button::render(){
-    
+    //If the image variable was able to load something, draw that image
     if (image.IsValid()){
         image.Draw(this->x, this->y, WHITE);
     }
-
+    //If the image variable was not able to load something, draw a rectangle in its place
     else {
         raylib::Rectangle(this->x,this->y,this->width,this->height).Draw(RED);
     }
@@ -30,7 +32,10 @@ void Button::render(){
 }
 
 void Button::tick(){
-    
+    //We will call this function every single frame in Program::Update();
+    //and in every call, if isPressed is true then we will decrease 
+    //the pressCounter variable 1 by 1 until pressCounter reaches 0, 
+    //we will then set the buttin as not pressed
     if(isPressed){
         pressCounter--;
         if(pressCounter == 0){
@@ -38,20 +43,21 @@ void Button::tick(){
             pressCounter = -1;
         }
     }
-    
+    //FUNFACT; each called frame called can also be known as a "tick"
+    //And 60 ticks is the same as 1 second. (That's why they call it 60fps in games)
+    //Might differ a bit from one computer to another, but it's a good rule of thumb
 }
 
 void Button::playSound(){
-    //If the sound path was correct, and the buttonSound variable has a
-    //sound loaded, then play it
+    //If the buttonSound variable has a sound loaded, then play it
     if (buttonSound.IsValid()) {
         buttonSound.Play();
     }
 }
 
 void Button::setPressed(){
-    //If the mouse is inside the button, set the button as pressed
-    if (raylib::Rectangle(this->x, this->y, this->width, this->height).CheckCollision(raylib::Mouse::GetPosition())) {
+    //If the mouse is inside the button(a rectangle created using its properties), set the button as pressed
+    if (CheckCollisionPointRec(GetMousePosition(),Rectangle {this->x,this->y,this->width,this->height})) {
         isPressed = true;
         pressCounter = 1; //You may change that pressCounter variable to a higher number
         //If you wish for it to stay pressed for a longer time
@@ -59,6 +65,7 @@ void Button::setPressed(){
 }
 
 Button::~Button(){
+    //Unload the button resources once it goes out of scope
     UnloadTexture(image);
     UnloadSound(buttonSound);
 }
